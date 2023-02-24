eea290b7a97ff678615eb80cafbeb94fce007c4a
taras-sereda <taras.y.sereda@proton.me>
2023-02-24 13:19:00 +0200

    tempterature and nucleus sampling

--- gpt2_pico.py
+++ gpt2_pico.py
@@ -1,4 +1,5 @@
 import numpy as np
+import bisect
 
 def gelu(x):
     return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * x**3)))
@@ -40,20 +41,37 @@ def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):
         x = transformer_block(x, **block, n_head=n_head)
     return layer_norm(x, **ln_f) @ wte.T
 
-def generate(inputs, params, n_head, n_tokens_to_generate):
+def generate(inputs, params, n_head, n_tokens_to_generate, sampling, cum_prob: float):
     from tqdm import tqdm
     for _ in tqdm(range(n_tokens_to_generate), "generating"):
         logits = gpt2(inputs, **params, n_head=n_head)
-        next_id = np.argmax(logits[-1])
+        if sampling == 'temp':
+            # with higher temp probabilities distribution is getting more even, uncertainty increases.
+            next_id = np.argmax(softmax(logits[-1]/1e7))
+        elif sampling == 'top_p':
+            # nucleus sampling
+            probs = softmax(logits[-1])
+
+            sorted_probs = np.sort(probs)[::-1]
+            sorted_idxs = np.argsort(probs)[::-1]
+
+            nucleus_idx = bisect.bisect_right(np.cumsum(sorted_probs), cum_prob) + 1
+            high_prob_idxs = sorted_idxs[:nucleus_idx]
+            np.random.shuffle(high_prob_idxs)
+            next_id = high_prob_idxs[0]
+
+        else:
+            next_id = np.argmax(logits[-1])
+
         inputs.append(int(next_id))
     return inputs[len(inputs) - n_tokens_to_generate :]
 
-def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", models_dir: str = "models"):
+def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", models_dir: str = "models", sampling=None, cum_prob=None):
     from utils import load_encoder_hparams_and_params
     encoder, hparams, params = load_encoder_hparams_and_params(model_size, models_dir)
     input_ids = encoder.encode(prompt)
     assert len(input_ids) + n_tokens_to_generate < hparams["n_ctx"]
-    output_ids = generate(input_ids, params, hparams["n_head"], n_tokens_to_generate)
+    output_ids = generate(input_ids, params, hparams["n_head"], n_tokens_to_generate, sampling, cum_prob)
     output_text = encoder.decode(output_ids)
     return output_text
 
